b6e52c3c4a9998b0439834b22646d0b1c441abd5
Randy Caballero <caballerorandy6@gmail.com>
2022-09-12 23:51:19 -0500

    hasta la clase 38

---
+++ 03-bases-node/config/yargs.js
@@ -0,0 +1,33 @@
+const argv = require("yargs")
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+    describe: "Es la base de la tabla de multiplicar",
+  })
+  .option("h", {
+    alias: "hasta",
+    type: "number",
+    default: 10,
+    describe: "Este es el numero hasta donde quieres la tabla",
+  })
+  .option("l", {
+    alias: "listar",
+    type: "boolean",
+    default: false,
+    describe: "Muestra la tabla en consola",
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) {
+      throw "La base tiene que ser un numero";
+    }
+    return true;
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.h)) {
+      throw "El tamano de la tabla tiene que ser un numero";
+    }
+    return true;
+  }).argv;
+
+module.exports = argv;
--- 04-bases-node/app.js
+++
@@ -1,9 +0,0 @@
-const { crearArchivo } = require("./helpers/multiplicar");
-
-console.clear();
-
-const base = 6;
-
-crearArchivo(base)
-  .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
-  .catch((err) => console.log(err));
